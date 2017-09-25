3ee2e1cb26dd5d3aea0e7467d80ef763387dd64d
iam <iam@100am.co>
2017-09-25 22:34:00 +0300

    updated package

--- app/src/main/java/iam/thevoid/cursorobservablelist/MainActivity.java
+++ app/src/main/java/iam/thevoid/cursorobservablelist/MainActivity.java
@@ -1,7 +1,10 @@
 package iam.thevoid.cursorobservablelist;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import iam.thevoid.columnbinder.CursorObservableList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Cursor query = getContentResolver().query(null, null, null, null, null);
+        CursorObservableList<Pojo> list =
+                new CursorObservableList<>(Pojo.class, query);
     }
 }
---
+++ app/src/main/java/iam/thevoid/cursorobservablelist/Pojo.java
@@ -0,0 +1,16 @@
+package iam.thevoid.cursorobservablelist;
+
+import android.provider.ContactsContract;
+
+import iam.thevoid.annotation.ColumnName;
+
+/**
+ * Created by iam on 25/09/2017.
+ */
+
+public class Pojo {
+    @ColumnName(name = ContactsContract.CommonDataKinds.Email.DATA)
+    String mName;
+    @ColumnName(name = ContactsContract.Contacts.DISPLAY_NAME)
+    String mEmail;
+}
